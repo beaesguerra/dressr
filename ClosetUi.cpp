03ae04e97f9b3d6af4c9a2acd1be591e8d9b2d7e
Bea <bea.esguerra@hotmail.com>
2016-02-17 10:39:11 -0700

    a

--- ClosetUi.cpp
+++ ClosetUi.cpp
@@ -1,10 +1,11 @@
 #include "ClosetUi.h"
 #include "ui_ClosetUi.h"
-
+#include "QLineEdit"
 ClosetUi::ClosetUi() 
 : ui(new Ui::ClosetUi)
 {
     ui->setupUi(this);
+    //ui->filters->lineEdit()->setPlaceholderText("Filter by:");
 }
 
 ClosetUi::~ClosetUi()
